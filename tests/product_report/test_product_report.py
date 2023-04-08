094799a841bc0e3d847330b882c8e616dc797f80
Marcelo Ataíde <msa.trybe@gmail.com>
2023-04-08 13:24:19 -0300

    Requisito 8 - Testa o relatório individual do produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        'Produto 01',
+        'Empresa 01',
+        '20/10/2022',
+        '24/10/2022',
+        '0001',
+        'instrucoes_de_armazenamento',
+    )
+
+    assert product.id == 1
+    assert product.nome_do_produto == 'Produto 01'
+    assert product.nome_da_empresa == 'Empresa 01'
+    assert product.data_de_fabricacao == '20/10/2022'
+    assert product.data_de_validade == '24/10/2022'
+    assert product.numero_de_serie == '0001'
+    assert product.instrucoes_de_armazenamento == 'instrucoes_de_armazenamento'
